6ee366c095014536d2fbf4d8ec72feda8734b937
James Rungsawang <jamesrngsawang@gmail.com>
2020-10-16 23:55:01 -0700

    Shift left code-e solution
    Signed-off-by: James Rungsawang <jamesrngsawang@gmail.com>

---
+++ Contest_Solutions/Code-e/Contest_06/Shift_Left/main.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+
+int main(){
+    ll t;
+    cin>>t;
+    while(t--){
+        ll a, b;
+        cout<<(a<<b)<<endl;
+    }
+    return 0;
+}
