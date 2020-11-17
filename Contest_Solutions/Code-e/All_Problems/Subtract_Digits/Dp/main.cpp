34502ee2ce0df81a0b65ce4f41e44c30968be56a
James Rungsawang <jamesrngsawang@gmail.com>
2020-11-17 12:30:58 -0800

    Subtract digits had the wrong solution code, fixed

--- Contest_Solutions/Code-e/All_Problems/Subtract_Digits/Dp/main.cpp
+++ Contest_Solutions/Code-e/All_Problems/Subtract_Digits/Dp/main.cpp
@@ -1,21 +1,25 @@
 #include<bits/stdc++.h>
 typedef long long ll;
 using namespace std;
+const ll INF = 1e18;
 
 int main(){
-    int n;
-    cin>>n;
-    vector<ll> dp(n+1, INF);
-    dp[n] = 0;
-    for(int i = n; i; i--){
-        ll temp = i;
-        while(temp){
-            ll x = temp % 10;
-            dp[i-x] = min(dp[i-x], dp[i]+1);
-            temp /= 10;
+    int t;
+    cin>>t;
+    while(t--){
+        int n;
+        cin>>n;
+        vector<ll> dp(n+1, INF);
+        dp[n] = 0;
+        for(int i = n; i; i--){
+            ll temp = i;
+            while(temp){
+                ll x = temp % 10;
+                dp[i-x] = min(dp[i-x], dp[i]+1);
+                temp /= 10;
+            }
         }
+        cout<<dp[0]<<endl;
     }
-    cout<<dp[0];
-    
     return 0;
 }
