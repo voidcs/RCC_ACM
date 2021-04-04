604da5097d46fb25633c6658d66db3a831dec830
voidcs <jamesrngsawang@gmail.com>
2021-04-03 23:49:37 -0700

    Pair to powers of two test case generator

---
+++ Contest_Creation_Tools/Test_Case_Generation/Pair_To_Powers_Of_Two/main.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+const ll INF = 1e18;
+const ll mod = 1e9+7;
+
+int main(){
+    freopen("input.txt", "w", stdout);
+    random_device rd;  
+    mt19937_64 eng(rd()); 
+    uniform_int_distribution<long long> rand;
+    
+    unordered_map<int, int> m;
+    int n = rand(eng) % (int)2e5;
+    vector<int> v(n);
+    for(int i = 0; i < n; i++)
+        v[i] = rand(eng) % (int)1e4 + 1;
+    cout<<n<<endl;
+    for(int x: v)
+        cout<<x<<" ";
+    cout<<endl;
+    
+    unordered_set<int> powersOfTwo;
+    for(int i = 0; i <= 30; i++){
+        powersOfTwo.insert(1<<i);
+    }
+    int ans = 0;
+    for(int i = 0; i < n; i++){
+        for(auto x: powersOfTwo){
+            int need = x - v[i];
+            if(m.count(need)){
+                ans += m[need];
+            }
+        }
+        m[v[i]]++;
+    }
+    
+    freopen("output.txt", "w", stdout);
+    cout<<ans<<endl;
+    
+    return 0;
+}
