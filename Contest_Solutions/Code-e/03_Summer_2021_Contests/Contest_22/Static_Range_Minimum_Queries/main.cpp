3d8ac3513340876310379aaa44c5e9691eda1150
voidcs <jamesrngsawang@gmail.com>
2021-08-22 14:59:51 -0700

    Contest 22 solutions

---
+++ Contest_Solutions/Code-e/03_Summer_2021_Contests/Contest_22/Static_Range_Minimum_Queries/main.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+const ll mod = 1e9+7;
+const ll INF = 1e18;
+ 
+int main(){
+    int n, q;
+    cin>>n>>q;
+    vector<int> v(n);
+    for(int &x: v)
+        cin>>x;
+    vector<vector<int>> st(20, vector<int>(n));
+    for(int i = 0; i < n; i++){
+        st[0][i] = v[i];
+    }
+    for(int j = 1; j < 20; j++){
+        for(int i = 0; i < n; i++){
+            if(i + (1<<j) <= n){
+                st[j][i] = min(st[j-1][i], st[j-1][i + (1<<(j-1))]);
+            }
+        }
+    }
+    while(q--){
+        int a, b;
+        cin>>a>>b;
+        a--, b--;
+        int i = log2(b-a+1);
+        int ans = min(st[i][a], st[i][b-(1<<i)+1]);
+        cout<<ans<<endl;
+    }
+    return 0;
+}
